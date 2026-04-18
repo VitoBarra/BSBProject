70c515023b72e419f651128efeeaec9e1e787754
Vito Barra <vb.vitobarra@gmail.com>
2026-04-18 18:53:00 +0200

    chore: add homework folder

---
+++ Homework/Homework1.py
@@ -0,0 +1,11 @@
+﻿"""
+ Consider the TP53 gene in human. From NCBI Gene get the genomic sequence. Retrieve the list of protein sequences derived from each isoform.
+ • For the same gene, get from NCBI RefSeq the coding sequence and the protein sequences for human, chimp and mouse.
+ • In Ensembl Genome Browser visualize the information related to the human TP53 gene – The longest transcript and get the coding sequence;
+    – How many splice variants do you find for this gene? How many protein coding?
+    – Get the evolutionary tree of TP53
+    – Find a germline (identified by rs prefix) and somatic (identified with COSM prefix) SNP occurring within the coding sequence of the gene.
+    – Get the 3’UTR and the 5’UTR sequences for this gene
+"""
+
+
