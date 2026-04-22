36866b9549f6cfff4d1009ab1a66fb2d7843aca8
Vito Barra <vb.vitobarra@gmail.com>
2026-04-22 19:53:46 +0200

    feat: add fastp trimming step

--- Project/main.py
+++ Project/main.py
@@ -4,6 +4,7 @@ import argparse
 from pathlib import Path
 
 from DataSourcer import DataSourceConfig, PROFILES, build_metadata_table
+from QualityControl import QualityControlConfig, run_fastp, run_fastqc, run_multiqc
 from DataSourcer.download_fastq import download_fastq_from_tsv
 
 
@@ -22,12 +23,18 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--soft-path", type=Path, default=None, help="Optional custom path for the GEO SOFT file.")
     parser.add_argument("--metadata-path", type=Path, default=None, help="Optional custom output/input TSV path.")
 
-    parser.add_argument("--run-fastqc", default= True, action="store_true", help="Run FastQC on raw reads")
+    parser.add_argument("--run-fastqc", default= True, action="store_true", help="Run FastQC on raw reads and, if trimming is enabled, also on trimmed reads.")
     parser.add_argument("--fastqc-threads", type=int, default=6, help="Worker threads to pass to FastQC.")
     parser.add_argument("--fastq-dest", type=Path, default=None, help="Optional custom destination directory for FASTQ files.")
     parser.add_argument("--fastqc-report-out", type=Path, default=None, help="Optional FastQC output directory for raw reads.")
+    parser.add_argument("--fastqc-trimmed-report-out", type=Path, default=None, help="Optional FastQC output directory for trimmed reads.")
 
-    parser.add_argument("--run-multiqc", default=True, action="store_true", help="Run MultiQC on all QC results, including FastQC.")
+    parser.add_argument("--run-fastp", default= False, action="store_true", help="Run fastp on the FASTQ files after raw FastQC.")
+    parser.add_argument("--trimmed-fastq-dest", type=Path, default=None, help="Optional custom destination directory for trimmed FASTQ files.")
+    parser.add_argument("--fastp-report-out", type=Path, default=None, help="Optional fastp report directory.")
+    parser.add_argument("--fastp-threads", type=int, default=6, help="Worker threads to pass to fastp.")
+
+    parser.add_argument("--run-multiqc", default=True, action="store_true", help="Run MultiQC on all QC results, including FastQC and fastp reports.")
     parser.add_argument("--multiqc-report-out", type=Path, default=None, help="Optional MultiQC output directory.")
     return parser.parse_args()
 
@@ -63,6 +70,20 @@ def main() -> int:
         run_fastqc(
             qc_config,
             threads=args.fastqc_threads,
+            use_trimmed_reads=False
+        )
+
+    if args.run_fastp:
+        run_fastp(
+            qc_config,
+            threads=args.fastp_threads,
+        )
+
+    if args.run_fastqc:
+        run_fastqc(
+            qc_config,
+            threads=args.fastqc_threads,
+            use_trimmed_reads=True,
         )
 
     if args.run_multiqc:
